4ee86d8cfa78cada0671d233d1eb7d42cbd1ebc8
Anjihee <jihee5100@sookmyung.ac.kr>
2023-08-03 21:40:07 +0900

    [0803] 지희:마이페이지 + 카테고리 2차 업로드

---
+++ app/src/main/java/com/jihee/testver2/MainActivity_mypage.kt
@@ -0,0 +1,12 @@
+package com.jihee.testver2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity_mypage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+
+    }
+}
